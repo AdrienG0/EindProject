0221d1370678e16a82e14066161fc1835ef27b9c
AdrienG0 <145970282+AdrienG0@users.noreply.github.com>
2025-11-26 14:17:19 +0100

    Initial commit

---
+++ src/main/java/com/example/eindproject/EindProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.eindproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EindProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eindproject/EindProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eindproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
