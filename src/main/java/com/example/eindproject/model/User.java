986b737ef3f193d2429b3b25f46072695c786c6a
AdrienG0 <145970282+AdrienG0@users.noreply.github.com>
2025-11-28 15:35:02 +0100

    User klasse uitbreiden

--- src/main/java/com/example/eindproject/model/User.java
+++ src/main/java/com/example/eindproject/model/User.java
@@ -13,8 +13,14 @@ public class User {
 
     private String name;
 
-    @Column(unique = true)
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
 
     @OneToMany(mappedBy = "user")
     private List<Order> orders = new ArrayList<>();
@@ -31,6 +37,10 @@ public class User {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
@@ -45,6 +55,20 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
     }
 
     public List<Order> getOrders() {
