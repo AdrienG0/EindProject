18fdccc48af8ea20972b6ab7df49d1ed488c64c9
AdrienG0 <145970282+AdrienG0@users.noreply.github.com>
2025-11-28 11:27:00 +0100

    Create DataInitializer.java
    Gebruikt om database te vullen met Dummy data

---
+++ src/main/java/com/example/eindproject/config/DataInitializer.java
@@ -0,0 +1,15 @@
+package com.example.eindproject.config;
+
+import com.example.eindproject.model.*;
+import com.example.eindproject.repository.*;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.math.BigDecimal;
+
+@Configuration
+public class DataInitializer {
+
+
+}
